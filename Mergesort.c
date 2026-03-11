3c728cdfe5492b09afcc927e6d5ce34320df78c0
dhananjaychoudharycs24-lab <dhananjaychoudhary.cs24@bmsce.ac.in>
2026-03-11 10:40:47 +0530

    Mergesort.c

--- Mergesort.c
+++ Mergesort.c
@@ -1,72 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-void merge(int a[], int low, int mid, int high)
-{
+void merge(int arr[], int l, int m, int r) {
     int i, j, k;
-    int temp[1000];
+    int n1 = m - l + 1;
+    int n2 = r - m;
 
-    i = low;
-    j = mid + 1;
-    k = low;
+    int L[n1], R[n2];
 
-    while(i <= mid && j <= high)
-    {
-        if(a[i] <= a[j])
-        {
-            temp[k] = a[i];
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
             i++;
-        }
-        else
-        {
-            temp[k] = a[j];
+        } else {
+            arr[k] = R[j];
             j++;
         }
         k++;
     }
 
-    while(i <= mid)
-    {
-        temp[k] = a[i];
+    while (i < n1) {
+        arr[k] = L[i];
         i++;
         k++;
     }
 
-    while(j <= high)
-    {
-        temp[k] = a[j];
+    while (j < n2) {
+        arr[k] = R[j];
         j++;
         k++;
     }
-
-    for(i = low; i <= high; i++)
-        a[i] = temp[i];
 }
 
-void mergesort(int a[], int low, int high)
-{
-    int mid;
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = (l + r) / 2;
 
-    if(low < high)
-    {
-        mid = (low + high) / 2;
-        mergesort(a, low, mid);
-        mergesort(a, mid + 1, high);
-        merge(a, low, mid, high);
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
     }
 }
 
-int main()
-{
-    int a[1000], n, i;
+int main() {
+    int n, i;
+    int *arr;
+    clock_t start, end;
+    double time_taken;
+
     printf("Enter number of elements: ");
     scanf("%d", &n);
-    printf("Enter elements:\n");
-    for(i = 0; i < n; i++)
-        scanf("%d", &a[i]);
-    mergesort(a, 0, n-1);
-    printf("Sorted elements are:\n");
-    for(i = 0; i < n; i++)
-        printf("%d ", a[i]);
+
+    arr = (int *)malloc(n * sizeof(int));
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    start = clock();
+
+    mergeSort(arr, 0, n - 1);
+
+    end = clock();
+
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\nTime taken to sort: %f seconds\n", time_taken);
+
+    free(arr);
     return 0;
 }
